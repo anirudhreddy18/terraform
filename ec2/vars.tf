1f4d042e4afae5d8f8f0744c2c00166914822b98
Anirudh Vemula <anirudh.anirudh15@gmail.com>
2020-06-06 17:10:21 -0500

    commit

--- ec2/vars.tf
+++ ec2/vars.tf
@@ -2,13 +2,13 @@ variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-    default = "us-east-2"
+  default = "us-east-2"
 }
 
 variable "AMIS" {
-    type = "map" 
-    default = {
-        "us-east-2" = "ami-0f7919c33c90f5b58"
-    }
+  type = "map"
+  default = {
+    "us-east-2" = "ami-0f7919c33c90f5b58"
+  }
 }
 
